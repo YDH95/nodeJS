fa610b136f4e2a442bc5e826d2b74ff1477dc51e
YDH95 <wowhky81234@gmail.com>
2022-08-17 12:20:17 +0900

    백엔드 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+    id : ["test","test2"],
+    psword : [1234, 12345]
+}
+
 const output = {
     home : (req, res) => {
         res.render("home/index");
@@ -12,11 +17,27 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        console.log(req);
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+
+            if (users.psword[idx] == psword) {
+                return res.json({
+                    success : true,
+                });
+            }
+        }
+
+        return res.json({
+            success : false,
+            msg : "login false"
+        })
     }
 }
 
 module.exports = {
-    outpu,
+    output,
     process
 }
